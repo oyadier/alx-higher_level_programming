89c0b94f0725c9c6f0561aca03fdfb01606ee5c0
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-10-04 15:58:19 +0000

    To Upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(0, len(str)):
+        ch = ord(str[i])
+        if (ch > 96) and (ch < 123):
+            ch -= 32
+        print("{:c}".format(ch), end="")
+    print("")
+
+uppercase("beST")
+
