24d95a08d6de747aaac8fd7026d8d2520784851f
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-10-06 05:58:59 +0000

    Infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import hidden_4
-
+import sys
 if __name__ == "__main__":
-    list = dir(hidden_4)
-    n = len(dir(hidden_4))
-    for i in range(n):
-        if list[i][:2] != "__":
-            print(list[i])
+    n = len(sys.argv)
+    sum = 0
+    for i in range(1, n):
+        sum += int(sys.argv[i])
+    print(sum)
