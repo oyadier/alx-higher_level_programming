add8c8cc69146d53d562b4165b15a315d75b749a
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-10-09 12:46:45 +0000

    Return Tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,12 +1,14 @@
 #!/bin/python3
 def multiple_returns(sentence):
-    """ multiple_returns - a function that returns a 
+
+    """multiple_returns - a function that returns a
         tuple with the length of a string and its first character.
+
         @sentence: the string to be parse
+
         Return: a tuple with string lengh and first string char
     """
-
-    lengh= 0
+    lengh = 0
     for i in sentence:
         lengh += 1
     if lengh == 0:
