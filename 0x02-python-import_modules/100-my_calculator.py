6690b4ee166b00664b79fa47efcf0c938dd4d659
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-10-10 13:51:52 +0000

    My calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,25 +4,19 @@ import sys
 
 if __name__ == "__main__":
 
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 4:
         first = int(sys.argv[1])
         second = int(sys.argv[3])
         match (sys.argv[2]):
             case "+":
                 print("{} + {} = {}".format(first, second, add(first, second)))
-                sys.exit("0")
             case "-":
                 print("{} - {} = {}".format(first, second, sub(first, second)))
-                sys.exit("0")
             case "*":
                 print("{} * {} = {}".format(first, second, mul(first, second)))
-                sys.exit("0")
             case "/":
                 print("{} / {} = {}".format(first, second, div(first, second)))
-                sys.exit("0")
             case _:
                 print("Unknown operator. Available operators: +, -, * and /")
-                sys.exit("1")
     else:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit("1")
