94369088908d16f45a7df45dd419b40231458704
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-10-10 12:50:28 +0000

    My calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+
+if __name__ == "__main__":
+
+    if len(sys.argv) == 4:
+        first = int(sys.argv[1])
+        second = int(sys.argv[3])
+        match (sys.argv[2]):
+            case "+":
+                print("{} + {} = {}".format(first, second, add(first, second))) 
+            case "-":
+                print("{} - {} = {}".format(first, second, sub(first, second)))
+            case "*":
+                print("{} * {} = {}".format(first, second, mul(first, second)))
+            case "/":
+                print("{} / {} = {}".format(first, second, div(first, second)))
+            case _:
+                print("Unknown operator. Available operators: +, -, * and /")
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
