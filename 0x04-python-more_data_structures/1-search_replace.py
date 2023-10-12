341a7ba5ff7c3609940d99358f4df9d5b7dac1a8
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-10-12 02:05:40 +0000

    Find and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,10 +5,3 @@ def search_replace(my_list, search, replace):
         if cp[i] == search:
             cp[i] = replace
     return cp
-
-
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_list)
