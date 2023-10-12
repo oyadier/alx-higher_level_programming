4f0b0c59e831457f460845f1509da8a901e9a88c
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-10-12 03:00:35 +0000

    Dictionary ordered key

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for k , v in sorted(a_dictionary.items()):
-        print(f"{k}: {v}")
-
-
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
-print_sorted_dictionary(a_dictionary)
+        print(f"{k}: {v}"))
