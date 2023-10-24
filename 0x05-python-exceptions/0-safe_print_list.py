94a41abea000001e8dffed6e737836d7c7b1fe45
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-10-24 05:29:54 +0000

    Code style

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end="")
             _len += 1
-        print(" ")
     except IndexError:
-        print(" ")
-
+        pass
+    print();
     return _len
