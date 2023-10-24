becfa1d17d532501e30446f202b73f59423be8b0
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-10-24 13:15:20 +0000

    Divide List by list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
+
 def list_division(my_list_1, my_list_2, list_length):
-    result_list = []
-    for idx in range(list_length):
+    """
+        Divide a list by list
+    """
+    div_list = []
+    for i in range(list_length):
         try:
-            result_list.append(my_list_1[idx] / my_list_2[idx])
-        except(TypeError, ZeroDivisionError, IndexError) as err:
-            if (isinstance(err, ZeroDivisionError)):
-                print("division by 0")
-            elif (isinstance(err, TypeError)):
-                print("wrong type")
-            elif (isinstance(err, IndexError)):
-                print("out of range")
-            result_list.append(0)
+            div_list.append(my_list_1[i] / my_list_2[i])
+        except TypeError:
+            print("wrong type")
+            div_list.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            div_list.append(0)
+        except IndexError:
+            print("out of range")
+            div_list.append(0)
         finally:
             pass
-    return result_list
+    return div_list
