2a4ada701955c7c1310811f02ca1b3a2c7a34329
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-10-25 05:07:19 +0000

    Instance Options

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Define a new Square class"""
+
+
+class Square():
+
+    """Placing methods here"""
+    def __init__(self, size=0):
+        '''Creating an object
+            Args:
+            size (int): A positive or 0
+        '''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be an >= 0")
+        self.__size = size
