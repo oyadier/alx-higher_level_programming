760b4532e0d747ff987dad48c007d39b86671711
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-10-25 05:10:15 +0000

    Instance Options

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,8 +11,8 @@ class Square:
             Args:
             size (int): A positive or 0
         '''
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must be an >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
