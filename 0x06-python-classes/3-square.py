9c9cacd448f11be9a45615f65f6ab084821d771f
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-10-25 05:20:40 +0000

    Return Square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Define a new Square class"""
+
+
+class Square:
+
+    """Placing methods here"""
+    def __init__(self, size=0):
+        '''Creating an object
+            Args:
+            size (int): A positive or 0
+        '''
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        '''The get method for the square
+        '''
+        return self.__size ** 2
