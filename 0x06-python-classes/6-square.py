77ad7a318763ffd3cf8a1acb1c4869c64733088b
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-10-26 02:48:30 +0000

    Position variable

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+"""Define a new Square class"""
+
+
+class Square:
+
+    """Placing methods here"""
+    def __init__(self, size=0, position=(0, 0)):
+        '''Creating an object
+            Args:
+            size (int): A positive or 0
+        '''
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        if len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        '''The get method for the square
+        '''
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''A setter method to set instance private size
+            Args:
+            value (int): a posive or zero value
+        '''
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        '''prints in stdout the square with the character # or a new line
+            is size is zero.
+        '''
+        if self.__size == 0:
+            print()
+            return
+
+        for line in range(self.__position[1]):
+            print()
+        for col in range(self.__size):
+            print("{}{}".format(" " * self.__position[0], \ "#" * self.__size))
