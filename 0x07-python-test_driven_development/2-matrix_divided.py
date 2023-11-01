ef176814ef07d9198f51e5807194279a582fca67
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-11-01 07:04:41 +0000

    pycodestyle

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -10,13 +10,15 @@ def matrix_divided(matrix, div):
 
     for i in range(len(matrix)):
         if not isinstance(matrix[i], list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(
+                    "matrix must be a matrix (list of lists) of integers/floats")
 
         if len(matrix[0]) != len(matrix[i]):
             raise TypeError("Each row of the matrix must have the save size")
 
         for x in matrix[i]:
             if not isinstance(x, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(
+                        "matrix must be a matrix (list of lists) of integers/floats")
             new_list.append(round(x / div, 2))
     return new_list
