ff28365781361f869c6983a6ba6d873769626d8b
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-11-01 17:38:55 +0000

    Pycode

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,6 +1,7 @@
 import numpy
 
-def lazy_matrix_mul(m_a, m_b):
-    """Callculate the matrx multiplication of two matrices Using numpy module"""
-    return numpy.matmul(m_a, m_b)
 
+def lazy_matrix_mul(m_a, m_b):
+    """Callculate the matrx multiplication of two\
+            matrices Using numpy module"""
+    return numpy.matmul(m_a, m_b)
