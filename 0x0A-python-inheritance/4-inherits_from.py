6db1aa7b93f937cbaf6e886138d9077a701cafdb
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-11-06 18:03:06 +0000

    is heritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -13,4 +13,4 @@ def inherits_from(obj, a_class):
             True: if it inherited
             False: if not
     """
-    return isinstance(obj, a_class)
+    return ((issubclass(type(obj), a_class)) and type(obj) != a_class)
