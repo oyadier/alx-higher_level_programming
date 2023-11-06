0ad51ae9e763f579632bf9c7e175f8d687fb00f7
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-11-06 13:51:55 +0000

    Is same

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
 def is_same_class(obj, a_class):
+    """
+    Check identical object
+    Args:
+        obj (object): first object
+        a_class: the second object
+
+    Return: True if same orderwise False
+    """
+
     if isinstance(obj.__repr__(), a_class):
         return True
     else:
