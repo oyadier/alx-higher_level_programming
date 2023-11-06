9c0b533e8f32cc9671d57e3fa5d377a43669ed97
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-11-06 18:27:00 +0000

    Doc

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-
 """Check two objects"""
-def is_same_class(obj, a_class):
-    """
-    Check identical object
-    Args:
-        obj (object): first object
-        a_class: the second object
 
-    Return: True if same orderwise False
+
+def is_same_class(obj, a_class):
+    """Check identical object
+        Args:
+            obj (object): first object
+            a_class (object): the second object
+
+        Return: True if same orderwise False
     """
-    return type(obj) ==  a_class
+    return type(obj) == a_class
