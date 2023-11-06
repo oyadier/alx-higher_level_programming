20f7a75d08f2ab23c05b819617f16ffc7afb666f
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-11-06 16:56:07 +0000

    Doc

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""Check two objects"""
 
 
 def is_same_class(obj, a_class):
     """
-    Check identical object
-    obj (object): first object
-    a_class: the second object
+    Check identical object.
+    obj (object): first object.
+    a_class: the second object.
 
-    Return: True if same orderwise False
+    Return: True if same orderwise False.
     """
-
     return isinstance(obj, a_class)
