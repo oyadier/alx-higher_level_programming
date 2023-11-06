e9b1eb1d86455b320477718d5cbf2074283468f1
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-11-06 17:24:30 +0000

    House keeping

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-'''Compare objects whether they are the same.'''
+'''This functino Compares objects whether they are the same.'''
+
 
 def is_king_of_class(obj, a_class):
     """
-    Check identical object.
+    Checks identical object.
 
     Args:
         obj (object): first object.
