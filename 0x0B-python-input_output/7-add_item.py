898034dfb73fcbe1764031eb283bb1b321d9b094
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-11-09 05:45:53 +0000

    Doc

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,11 +8,11 @@ load_from_json = __import__("8-load_from_json_file").load_from_json_file
 
 
 def add_item(args, filename):
-    '''
+    """
         adds items to a json file
         @args: The arguments that need to be added.
         @filename: The file that needs to be updated
-    '''
+    """
     try:
         content = load_from_json(filename)
     except Exception:
