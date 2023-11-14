2c39a9da82affb4978613f608ec912700be8426f
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-11-14 05:29:35 +0000

    Init file add

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -11,7 +11,17 @@ class TestRectangle(unittest.TestCase):
     """All available Test Case for each function"""
 
     def test_Id(self):
+        """Increase the id with subclass"""
         ob = Base()
         re = Rectangle(2, 3)
         self.assertEqual(re.id, 2)
 
+    def test_width_value(self):
+        ob = Base()
+        re = Rectangle(2, 3)
+        self.assertEqual(re.width, re.id)
+
+    def test_height_value(self):
+        Base._Base__nb_objects = 0
+        re = Rectangle(33, 9, 34)
+        self.assertEqual(re.id, re.__width - re.__x)
