d64d09fb8a7bd64e337e01733f2a49fce019ec01
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-11-14 06:37:43 +0000

    New Modification

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,148 +1,153 @@
 #!/usr/bin/python3
-"""Contains class `Rectangle` defination"""
+"""The Rectangel class"""
 from .base import Base
 
 
 class Rectangle(Base):
-    """Derives the class that inherits form `Base` class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initializes instance attributes
-        Args:
-            width (int): width of the ractangle
-            height (int): height of rectangle
-            x (int): x axis of rectangle
-            y (int): y axis of the rectangle
-            id (int): id of the rectangle
+        """Rectangle constructor
+            Args:
+                width (int): the width of the rectangle
+                height (int): the heigth of the rectangle
+                x (int): x value
+                y (int): y value
         """
-        self.validate_input(width=width, height=height, x=x, y=y)
+
+        self.check_values(width=width, height=height, x=x, y=y)
+
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
         super().__init__(id)
 
-    def __str__(self):
-        """Returns string representation of the instance"""
-        string = "[{}] ({}) {}/{} - {}"
-        if type(self) == Rectangle:
-            return "{}/{}".format(string.format(self.__class__.__name__,
-                                                self.id, self.__x, self.__y,
-                                                self.__width), self.__height)
-        return string.format(self.__class__.__name__, self.id, self.__x,
-                             self.__y, self.__width)
-
     @staticmethod
-    def validate_input(**kwargs):
-        """Validates the values before assigning them to
-        instance attributes
+    def check_values(**kwargs):
+        """Exception raiser for
+
+            Args:
+            kwargs (double-value): Key value pair object
         """
         for name, value in kwargs.items():
             if name == "x" or name == "y":
                 if type(value) != int:
                     raise TypeError("{} must be an integer".format(name))
+
                 elif value < 0:
                     raise ValueError("{} must be >= 0".format(name))
             else:
                 if type(value) != int:
-                    raise TypeError("{} must be an integer".format(name))
-                elif value <= 0:
-                    raise ValueError("{} must be > 0".format(name))
+                    raise TypeError("{} must be and integer".format(name))
+                if value <= 0:
+                    raise ValueError("{} must be >  0".format(name))
 
     @property
     def width(self):
-        """Retrieves the value of `width`
-        Returns:
-            value of width
-        """
-        return self.__width
-
-    @property
-    def height(self):
-        """Retrieves the value of `height`
-        Returns:
-            value of height
-        """
-        return self.__height
-
-    @property
-    def x(self):
-        """Retrieves the value of `x`
-        Returns:
-            value of x
-        """
-        return self.__x
-
-    @property
-    def y(self):
-        """Retrieves the value of `y`
-        Returns:
-            value of y
-        """
-        return self.__y
+        """Get the private width arribute"""
+        return __width
 
     @width.setter
     def width(self, value):
-        """sets the value of `width`"""
-        self.validate_input(width=value)
+        """modifying the value of the width
+            Args:
+                value (int): the new width value
+        """
+        self.check_values(width=value)
         self.__width = value
+
+    @property
+    def height(self):
+        """Get the private height arribute"""
+        return __height
 
     @height.setter
     def height(self, value):
-        """sets the value of `height`"""
-        self.validate_input(height=value)
+        """Modifying the value of the height
+            Args:
+                value (int): the new height value
+        """
+        self.check_values(height=value)
         self.__height = value
+
+    @property
+    def x(self):
+        """Get the value of x
+            Return:
+                x (int): x value
+        """
+        return self.__x
 
     @x.setter
     def x(self, value):
-        """sets the value of `x`"""
-        self.validate_input(x=value)
+        """Modify the value of x"""
+        self.check_values(x=value)
         self.__x = value
+
+    @property
+    def y(self):
+        """Get the value of y
+             Return:
+                int: y value
+        """
+        return self.__y
 
     @y.setter
     def y(self, value):
-        """sets the value of `y`"""
-        self.validate_input(y=value)
+        """Modify the value of y
+            Args:
+                value (int): the new integer value
+        """
+        self.check_values(y=value)
         self.__y = value
 
     def area(self):
-        """Calculates the area of `Rectangle` instance
-        Returns:
-            Calculated area of a Rectangle
+        """Compute the area of rectangle
+            Return:
+                int: area of rectangle
         """
-        return (self.__width * self.__height)
+        return self.__width * self.__height
 
     def display(self):
-        """Prints in stdout the Rectangle instance with the
-        character `#`
-        """
+        """Display number of width and height"""
         [print() for i in range(self.__y)]
         for i in range(self.__height):
             [print(" ", end="") for i in range(self.__x)]
-            for j in range(self.__width):
+            for i in range(self.__width):
                 print("#", end="")
             print()
 
+    def __str__(self):
+        """String Rep of Rectangle object"""
+
+        return f"[{self.__class__.__name__}] \
+ ({self.id}) {self.__x}/{self.__y} \
+ - {self.__width}/{self.__height}"
+
     def update(self, *args, **kwargs):
-        """Updates the values of the class"""
-        if args and len(args) > 0:
-            keys = ["id", "width", "height", "x", "y"]
-            for i, v in enumerate(args):
-                setattr(self, keys[i], v)
+
+        """No keyword argument
+            Args:
+                *args: pointer to an object
+                **kwargs: a pointer to objects
+        """
+        attributes = ['id', 'width', 'height', 'x', 'y']
+        if args is not None and len(args) > 0:
+            for i, value in enumerate(args):
+                setattr(self, attributes[i], value)
         else:
-            for k, v in kwargs.items():
-                setattr(self, k, v)
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def to_dictionary(self):
-        """Retrieves all the attributes of class to dictionary
-        Returns:
-            dictionary containing it's attributes
-        """
+        """Create dic of rectangle obj"""
+
         dictionary = {
                 "id": self.id,
-                "width": self.width,
-                "height": self.height,
-                "x": self.x,
-                "y": self.y
-        }
+                "width": self.__width,
+                "height": self.__height,
+                "x": self.__x,
+                "y": self.__y
+                }
+
         return dictionary
