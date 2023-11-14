b001a6d2c95aa4f9152d56ca48d2afec09bb7c61
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-11-14 05:14:52 +0000

    modify

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -3,10 +3,15 @@
 
 import unittest
 from models.rectangle import Rectangle
+from models.base import Base
 
 
 class TestRectangle(unittest.TestCase):
 
     """All available Test Case for each function"""
 
-    def 
+    def test_Id(self):
+        ob = Base()
+        re = Rectangle(2, 3)
+        self.assertEqual(re.id, 2)
+
