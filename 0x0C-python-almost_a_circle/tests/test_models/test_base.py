fbc9f6a6715e8ab0e29b005eb7fcf54868ce7273
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-11-16 06:44:14 +0000

    Handle Errors

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -198,18 +198,6 @@ class TestBase(unittest.TestCase):
             my_list = Base.from_json_string(read)
             self.assertDictEqual(r1.to_dictionary(), my_list[0])
 
-    def test_save_to_file_Square(self):
-        """Tests saving of json representation of objects to file"""
-        Base._Base__nb_objects = 0
-        r1 = Rectangle(10, 7, 2, 8)
-        s2 = Square(2)
-        Square.save_to_file([r1, s2])
-        with open("Square.json", "r") as file:
-            read = file.read()
-            my_list = Base.from_json_string(read)
-            self.assertDictEqual(r1.to_dictionary(), my_list[0])
-            self.assertDictEqual(s2.to_dictionary(), my_list[1])
-
     def test_save_to_file_Square_none(self):
         """Test saving to file with none"""
         Base._Base__nb_objects = 0
@@ -219,15 +207,6 @@ class TestBase(unittest.TestCase):
             my_list = Base.from_json_string(read)
             self.assertListEqual(my_list, [])
 
-    def test_save_to_file_Square_only_square(self):
-        """Test saving to file with only `Square`"""
-        Base._Base__nb_objects = 0
-        s1 = Square(1)
-        Square.save_to_file([s1])
-        with open("Square.json", "r") as file:
-            read = file.read()
-            my_list = Base.from_json_string(read)
-            self.assertDictEqual(s1.to_dictionary(), my_list[0])
 
     def test_create_Rectangle(self):
         """Testing creating of new `Rectangle` instance"""
@@ -237,45 +216,4 @@ class TestBase(unittest.TestCase):
         r2 = Rectangle.create(**r1_dictionary)
         self.assertFalse(r1 is r2)
         self.assertDictEqual(r1_dictionary, r2.to_dictionary())
-
-    def test_create_Square(self):
-        """Testing creating of new `Square` instance"""
-        Base._Base__nb_objects = 0
-        s1 = Square(3, 5, 1)
-        s1_dictionary = s1.to_dictionary()
-        s2 = Square.create(**s1_dictionary)
-        self.assertFalse(s1 is s2)
-        self.assertDictEqual(s1_dictionary, s2.to_dictionary())
-
-    def test_load_from_file_rectangle(self):
-        """Test loading from file for `Rectangle`"""
-        Base._Base__nb_objects = 0
-        r1 = Rectangle(10, 7, 2, 8)
-        r2 = Rectangle(2, 4)
-        list_rectangles_input = [r1, r2]
-        Rectangle.save_to_file(list_rectangles_input)
-        list_rectangles_output = Rectangle.load_from_file()
-        self.assertEqual(list_rectangles_input[0].__str__(),
-                         "[Rectangle] (1) 2/8 - 10/7")
-        self.assertEqual(list_rectangles_output[0].__str__(),
-                         "[Rectangle] (1) 2/8 - 10/7")
-        self.assertEqual(list_rectangles_input[1].__str__(),
-                         "[Rectangle] (2) 0/0 - 2/4")
-        self.assertEqual(list_rectangles_output[1].__str__(),
-                         "[Rectangle] (2) 0/0 - 2/4")
-
-    def test_load_from_file_square(self):
-        """Test loading from file for `Square`"""
-        s1 = Square(5)
-        s2 = Square(7, 9, 1)
-        list_squares_input = [s1, s2]
-        Square.save_to_file(list_squares_input)
-        list_squares_output = Square.load_from_file()
-        self.assertEqual(list_squares_input[0].__str__(),
-                         "[Square] (5) 0/0 - 5")
-        self.assertEqual(list_squares_output[0].__str__(),
-                         "[Square] (5) 0/0 - 5")
-        self.assertEqual(list_squares_input[1].__str__(),
-                         "[Square] (6) 9/1 - 7")
-        self.assertEqual(list_squares_output[1].__str__(),
-                         "[Square] (6) 9/1 - 7")
+ 
