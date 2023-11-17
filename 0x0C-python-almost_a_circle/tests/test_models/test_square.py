c1438bc2c01fb92fbea7dfaa9c73bd9571ebe073
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-11-17 04:37:10 +0000

    Square tests

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,16 @@
+"""Test cases for `Square` module"""
+
+import unittest
+
+from models.square import Square
+from models.rectangle import Rectangle
+from models.base import Base
+
+
+class TestSquare(unittest.TestCase):
+    """Defining all the possible cases"""
+
+    def test_size(self):
+        """Size is pass"""
+        sq = Square(1)
+        self.assertEqual(sq.size, 1)
