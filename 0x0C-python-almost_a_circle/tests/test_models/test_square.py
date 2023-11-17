e703cf364fe3d7e55d7c058558e8eda285565c5d
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-11-17 04:46:51 +0000

    Square tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -12,10 +12,20 @@ class TestSquare(unittest.TestCase):
 
     def test_size(self):
         """Size is pass"""
+        Base._Base__nb_objects = 0
         sq = Square(1)
         self.assertEqual(sq.size, 1)
 
     def test_id(self):
         """Size is pass"""
+        Base._Base__nb_objects = 0
         sq = Square(1)
-        self.assertEqual(sq.id, 1)
+        re = Rectangle(23, 3)
+        sq.id += 2
+        self.assertEqual(sq.id, 3)
+
+    def test_diff_size(self):
+        """Diff size values"""
+        Base._Base__nb_objects = 0
+        sq = Square(1, 2)
+        self.assertEqual(sq.size, 1)
