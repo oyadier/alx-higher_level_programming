1bac0b4c3c0cf96e21b18e312012ed8bcb50772f
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-12-14 05:53:15 +0000

    Order by score

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,2 @@
 -- Select records by condition
-SELECT score, name
-FROM second_table
-WHERE score >= 10
-ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
