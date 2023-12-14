ff0c41dc35d0a384301d47eb4ab3c806a02327c5
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-12-14 05:15:29 +0000

    Create table insert multiple

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- Create tb and insert multibles values
+CREATE TABLE IF NOT EXISTS second_table 
+(id INT, name VARCHAR(256), score INT);
+
+--Inserting values
+INSERT INTO second_table VALUES
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
