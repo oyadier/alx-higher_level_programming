fc8a3b2882eb2cf1cebe691e4260369689a36c28
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-12-15 04:40:04 +0000

    Create and grant permission

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- create db and user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXIST user_0d_2;
+CREATE USER IF NOT EXIST 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON *.* 'user_0d_2'@'localhost';
