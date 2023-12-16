bf79217641aefee9e53867e401980954586b1c67
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-12-16 06:23:50 +0000

    Null value

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -4,4 +4,4 @@ FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_show.id = tv_show_genre_id
 WHERE tv_show_genres.show_id IS NULL
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
