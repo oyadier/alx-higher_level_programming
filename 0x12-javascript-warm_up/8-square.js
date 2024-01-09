b5ba72b74e5292f51e351835b979d8930ca568b0
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2024-01-09 13:41:54 +0000

    Print square symbol

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const index = process.argv;
+let lop = 0;
+let x = 0;
+if (index.length < 3 || !isNaN(typeof (Number(index[2])))) {
+  console.log('Missing size');
+} else {
+  while (lop < index[2]) {
+    let sq = '';
+    for (x; x < index[2]; x++) {
+      sq += 'x';
+    }
+    console.log(sq);
+    x = 0;
+    lop++;
+  }
+}
