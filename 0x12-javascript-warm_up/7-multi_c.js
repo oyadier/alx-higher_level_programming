f30941c73dec728caf3027e52a2ad3a99a7ed395
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2024-01-09 12:58:57 +0000

    Loops

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const index = process.argv;
+let lop = 0;
+if (index.length < 3) {
+  console.log('Missing number of occurrences');
+} else if (index.length < 1) {
+  console.log('');
+} else {
+  while (lop < index[2]) {
+    console.log('C is fun');
+    lop++;
+ }
+}
