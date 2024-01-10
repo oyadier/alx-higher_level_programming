ccd9dbfbbf2e521980a2a34270268849a6c27498
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2024-01-10 03:13:20 +0000

    Extends

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,16 +2,15 @@
 const Sq = require('./5-square');
 class Square extends Sq {
   charPrint (c) {
-    if (typeof (c)  === 'undefined') {
-      super.print();
-    } else {
-      for (let x = 0; x < this.height; x++) {
-        let str = '';
-        for (let y = 0; y < this.width; y++) {
-          str += 'C';
-        }
-        console.log(str);
+    if (typeof (c) === 'undefined') {
+      c = 'X';
+    }
+    for (let x = 0; x < this.height; x++) {
+      let str = '';
+      for (let y = 0; y < this.width; y++) {
+        str += c;
       }
+      console.log(str);
     }
   }
 }
