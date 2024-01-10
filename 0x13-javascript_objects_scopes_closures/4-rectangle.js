23407272045ea511c4aa9c4584329ca03eaf0786
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2024-01-10 02:17:53 +0000

    More instance methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let x = 0; x < this.height; x++) {
+      let str = '';
+      for (let y = 0; y < this.width; y++) {
+        str += 'X';
+      }
+      console.log(str);
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
