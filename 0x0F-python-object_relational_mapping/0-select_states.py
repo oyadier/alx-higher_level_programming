1a3fe50fef7a13363637b242f0290faed243030e
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-01-14 05:21:22 +0000

    First Commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,36 @@
 #!/usr/bin/python3
 import sys
 import MySQLdb
-
 '''Using MySQLdb module to access and print rows in db'''
-arg = sys.argv
-usr = arg[1]
-password = arg[2]
-db_name = arg[3]
 
-con = MySQLdb.connect(host='localhost',
-                      port=3306,
-                      user=usr,
-                      passwd=password,
-                      db=db_name,
-                      charset="utf8")
-cur = con.cursor()
-cur.execute(f"SELECT * FROM `states` ORDER BY id ASC")
-all_row = cur.fetchall()
 
-for row in all_row:
-    print(row)
+def select_rows(username, password, db):
+    '''Using MySQLdb module to access and print rows in db
+        Args:
+            username (string): the name of the db user
+            passward (int): the db passcode
+            db (string): the name of the database
+    '''
+    con = MySQLdb.connect(host='localhost',
+                          port=3306,
+                          user=usr,
+                          passwd=password,
+                          db=db_name,
+                          charset="utf8")
+    cur = con.cursor()
+    cur.execute(f"SELECT * FROM `states` ORDER BY id ASC")
+    all_row = cur.fetchall()
 
-cur.close()
-con.close()
+    for row in all_row:
+        print(row)
+
+    cur.close()
+    con.close()
+
+
+if __name__ == "__main__":
+    arg = sys.argv
+    usr = arg[1]
+    password = arg[2]
+    db_name = arg[3]
+    select_rows(usr, password, db_name)
