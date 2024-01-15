62c6580ed9a28d17d3c6a742d82f832ddd246d19
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-01-15 03:40:28 +0000

    SQL Injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''Filter states by user input'''
+import MySQLdb
+import sys
+
+
+def search_by_input(username, password, name, searched):
+    '''Take argument and display state
+        Arg:
+            username (string): db user name
+            password (int): password of the db
+            name (string): db name
+            searched (string): keyword to search for
+    '''
+    con = MySQLdb.connect(host="localhost",
+                          port=3306,
+                          user=username,
+                          passwd=password,
+                          db=name)
+    cur = con.cursor()
+    query = "SELECT * FROM states WHERE\
+            name=%s ORDER BY id ASC"
+    cur.execute(query, (searched,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    con.close()
+
+
+if __name__ == "__main__":
+    arg = sys.argv
+    user = arg[1]
+    usr_passwd = arg[2]
+    db_name = arg[3]
+    search_ky = arg[4]
+    search_by_input(user, usr_passwd, db_name, search_ky)
