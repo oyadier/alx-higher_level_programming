c2601e614bed61498bf701138c8d74c52d11d1f2
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-01-15 14:43:54 +0000

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python3
+'''State class intance for SQLAlcamy'''
 import sqlalchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 class State(Base):
     '''State class'''
