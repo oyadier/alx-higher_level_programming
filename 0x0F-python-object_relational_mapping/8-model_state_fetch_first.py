b7c3519e93080c562b61b92803fd97312d62d61a
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-01-16 20:18:48 +0000

    Fetch only on state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    args = sys.argv
+    usr = sys.argv[1]
+    password = args[2]
+    db_name = args[3]
+    chemy_connect = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(chemy_connect.format(
+             usr, password, db_name), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State).first()
+
+    if query is not None:
+        print(f"{query.id}: {query.name}")
+    else:
+        print('Nothing')
