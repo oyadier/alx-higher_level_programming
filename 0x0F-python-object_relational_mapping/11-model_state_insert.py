1d7dc5e72974791d232a925c43ce4f742948b00f
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-01-18 20:59:37 +0000

    Add state object

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Add new state object"""
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv
+    user = args[1]
+    password = args[2]
+    db_name = args[3]
+    mysql_connect = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(mysql_connect.format(
+             user, password, db_name, port=3360))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = State(name='Narko')
+    session.add(state)
+    session.commit()
+    session.refresh(state)
+    print(state.id)
