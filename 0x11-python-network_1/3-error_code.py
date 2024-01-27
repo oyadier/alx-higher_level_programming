c72534f253ac0f76a7a4b75a1fd2d7101bcbd5e8
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-01-27 07:36:38 +0000

    Error Code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == "__main__":
     url = argv[1]
     try:
-        with request.openurl(url) as response:
-            print('Error code: ', response.code)
-    except Except as err:
-        print('Error code: ', e)
+        with urlopen(url) as response:
+            pass
+    except Exception as err
+        print('Error code: ', err.code)
