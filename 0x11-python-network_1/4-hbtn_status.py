07a0982d894efc48cd08afbab6dbcb305b12440b
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-01-27 15:23:47 +0000

    What my status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     response = requests.get('https://alx-intranet.hbtn.io/status',
                             auth=('user', 'pass'))
     str_type = response.text
-    print("Body response")
+    print("Body response:")
     print(f"\t- type: {type(str_type)}")
     print(f"\t- content: {str_type}")
