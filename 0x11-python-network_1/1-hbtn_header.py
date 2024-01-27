481fc50c9585b057e5e5c0b9836a63e56cca1ead
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-01-27 05:51:13 +0000

    Get the Request Id

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Get the request id from the url content"""
+import urllib.request
+import sys
+
+url = sys.argv[1]
+with urllib.request.urlopen(url) as resp:
+    content = resp.read()
+    response_id = resp.headers.get('X-Request-Id')
+    print(response_id)
