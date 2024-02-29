b74560dd8e6b5bb3b9f6a955de0b434a37776f93
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-02-29 06:00:10 +0000

    update

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,13 +1,10 @@
-//get list of title and list them
-$(function () {
-    $.ajax({
-      type: 'GET',
-      url: 'https://swapi.co/api/films/?format=json',
-      success: function (data) {
-        // console.log('success', data.results);
-        $.each(data.results, function (i, movie) {
-          $('UL#list_movies').append('<li>' + movie.title + '</li>');
+// get list of title and list them
+$(function() {
+    // Fetch data from the API
+    $.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json', function(data) {
+        // Iterate through each movie and add its title to the list
+        $.each(data.results, function(index, movie) {
+            $('#list_movies').append('<li>' + movie.title + '</li>');
         });
-      }
     });
-  });
+});
